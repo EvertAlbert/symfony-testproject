c80678b5567566f25a3e908c495455c2d160e7b0
Evert Albert <evert.albert@hotmail.be>
2022-04-04 14:54:54 +0200

    basic frontend

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,26 +2,17 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/")
      */
     public function indexAction(): Response
     {
-        return new Response('welcome home');
-    }
-
-    /**
-     * @Route ("/{slug}")
-     * @param $slug
-     * @return Response
-     */
-    public function home($slug): Response
-    {
-        return new Response('second page');
+        return $this->render('index.html.twig');
     }
 }
