0414f6916017f934f5c3522cc5796b899650196b
Evert Albert <evert.albert@hotmail.be>
2022-04-06 11:15:44 +0200

    working activity list

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\GroupActivity;
+use App\Repository\GroupActivityRepository;
+use Carbon\Carbon;
+use Couchbase\Group;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class CalendarController extends AbstractController
 {
     /**
      * @return Response
      */
-    public function indexAction(): Response
+    public function indexAction(GroupActivityRepository $groupActivityRepository): Response
     {
-        return $this->render('calendar/index.html.twig');
+//        $event1 = new GroupActivity();
+//        $event1
+//            ->setName('testevent 1')
+//            ->setStartDate(Carbon::now());
+//
+//        $event2 = new GroupActivity();
+//        $event2
+//            ->setName('event 2')
+//            ->setStartDate(Carbon::now())
+//            ->setEndDate(Carbon::now())
+//            ->setDescription('This is the description of the second event')
+//        ;
+//
+//        $groupActivityRepository->add($event1);
+//        $groupActivityRepository->add($event2);
+
+        $events = $groupActivityRepository->findAll();
+
+        return $this->render('calendar/index.html.twig', [
+            'events' => $events,
+        ]);
     }
 }
