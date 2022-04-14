400c365aac863dd2a061d7381f7c7ccb98e74fc3
Evert Albert <evert.albert@hotmail.be>
2022-04-14 16:50:19 +0200

    move photo album creation to admin route

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -33,40 +33,4 @@ class PhotoController extends AbstractController
             'photoAlbums' => $photoAlbums,
         ]);
     }
-
-    public function createAlbumAction(Request $request): Response
-    {
-        $newPhotoAlbum = $this->albumRepository->initiateAlbum();
-
-        $form = $this->createForm(PhotoAlbumType::class, $newPhotoAlbum);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $newPhotoAlbum = $form->getData();
-
-            // ... perform some action, such as saving the task to the database
-            $this->albumRepository->add($newPhotoAlbum);
-
-            return $this->redirectToRoute('photos');
-        }
-
-        return $this->renderForm('photos/create_album.html.twig', [
-            'form' => $form
-        ]);
-    }
-
-    public function temporaryUploadAction(Request $request)
-    {
-        /** @var UploadedFile $uploadedFile */
-        $uploadedFile = $request->files->get('image');
-        $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
-
-        $originalFileName = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-        $newFileName = Urlizer::urlize($originalFileName) . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
-        dd($uploadedFile->move(
-            $destination,
-            $newFileName,
-        ));
-    }
 }
